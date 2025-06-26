81fb641623218ba03ac9d3ef500e03b9339ba07f
mjmanash <jhamanash173@gmail.com>
2025-06-26 10:41:10 +0530

    handled cors

--- app/main.py
+++ app/main.py
@@ -4,8 +4,19 @@ from app.analyzer import ChatAnalyzer
 from app.auth import verify_api_key
 from fastapi import Depends
 from app.models import TranscriptInput
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["https://manash-biztelai-ds.streamlit.app"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
 processor = ChatProcessor("data/BiztelAI_DS_Dataset_V1.json")
 analyzer = ChatAnalyzer(processor.load_data())
 
